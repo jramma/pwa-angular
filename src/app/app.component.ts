2abb49c7a51abef38a9cfc368a54e2edbeb25d8f
jramma <jrmasip097@gmail.com>
2025-01-02 19:46:39 +0100

    funcion offline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,24 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-pwa-introduction';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadStyle('assets/css/animate.css');
+      this.loadStyle('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
+    }
+  }
+
+  loadStyle(url: string): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = url;
+      document.head.appendChild(link);
+    }
+  }
 }
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,21 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'image/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' }
+      ];
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
